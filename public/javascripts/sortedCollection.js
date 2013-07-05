dc82d18ba4d71f1911bbea89f9876a14f376ed74
Matthew Creager <mattcreager@goinstant.com>
2013-07-05 14:25:05 -0300

    Feature complete, time for refactooorrr

--- public/javascripts/sortedCollection.js
+++ public/javascripts/sortedCollection.js
@@ -18,6 +18,13 @@ SortedCollection.prototype = {
         return;
       }
 
+      self.collectionKey.on('remove', function(err, value, context) {
+        self.sortedCollection = [];
+
+        // fire add listeners on this object
+        self.fireEvents('remove', [null, value, context]); // err, new val, array, context
+      });
+
       // create a collection add listener
       self.collectionKey.on('add', function(key, value, context) {
 
@@ -94,12 +101,47 @@ SortedCollection.prototype = {
 
   add: function(value, cb) {
 
+    var self = this;
     var timestamp = new Date().getTime();
 
     this.collectionKey.add(timestamp, value, function(err, key) {
-      console.log(key);
+
+      if (err) {
+        cb(err);
+        return;
+      }
+
+      var stamp = key.name.split('/');
+
+      var set = {
+        stamp: timestamp,
+        key: key,
+        data: value
+      };
+
+      set.key.on('set', function(value, context) {
+        // new val then old val
+        self.fireEvents('change', [null, value, set.data, self.get(), context]);
+        this.data = value;
+      });
+
+      set.key.on('update', function(value) {
+        self.fireEvents('change', [null. value, set.data, self.get(), context]);
+        this.data = value;
+      });
+
+      self.sortedCollection.push(set);
+
+      self.sort();
+
+      cb.apply([null, key]);
     });
 
+  },
+
+  remove: function(cb) {
+    this.collectionKey.remove(cb);
+    this.sortedCollection = [];
   },
 
   get: function() {
@@ -134,28 +176,33 @@ function onLoad() {
     var sortedCollection = new SortedCollection(myCollection);
 
     sortedCollection.init(function(err, collection) {
-      console.log(collection.get());
+      console.log('initial collection', collection.get());
 
       collection.on('change', function(err, newVal, oldVal, entireArray, context) {
-        console.log(arguments);
+        console.log('on change:', arguments);
       });
 
       collection.on('add', function(err, newVal, oldVal, entireArray, context) {
-        console.log(arguments);
+        console.log('on add: ', arguments);
       });
 
-      collection.on('remove', function(err, oldVal, entireArray, context) {
-        console.log(arguments);
+      collection.on('remove', function(err, contextwindow) {
+        console.log('on remove event', arguments);
       });
 
-      collection.sortedCollection[5].key.set('Numero Two');
+      /*
+      collection.remove(function(err, value, context) {
+        console.log('remove collection ', arguments);
+      })*/
+
+      collection.sortedCollection[3].key.set('Numero Two');
     });
 
     /*
-    sortedCollection.add('Madame Mistress', function(err, val, entireArray, context) {
+    sortedCollection.add('Nany Drew', function(err, val, entireArray, context) {
       console.log(arguments);
-    });
-    */
+    });*/
+
 
   });
 
